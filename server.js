f7c52492c2d573c0e48c522201407d3e6fbb36b3
mohankumar27 <mohankumar27197@gmail.com>
2017-02-05 18:04:39 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -6,15 +6,15 @@ var app = express();
 app.use(morgan('combined'));
 
 app.get('/article-one', function (req, res) {
-  res.send('ariticle-one will be served in a while');
+  res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
 });
 
 app.get('/article-two', function (req, res) {
-  res.send('ariticle-two will be served in a while');
+  res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
 });
 
 app.get('/article-three', function (req, res) {
-  res.send('ariticle-three will be served in a while');
+  res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
 });
 
 app.get('/', function (req, res) {
