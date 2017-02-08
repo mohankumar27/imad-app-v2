265f15b4f636b9546f02b3b24a57de124ee0b0f9
mohankumar27 <mohankumar27197@gmail.com>
2017-02-08 19:14:39 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,7 @@
-
+var button=document.getElementById("click");
+button.onclick=function(){
+    var counter=0;
+    counter=counter+1;
+    var span=document.getElementById("count");
+    span.innerHtml=counter.toString();
+};
