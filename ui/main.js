ee37feda296cb6168974ce4c5980911bf61ecf04
mohankumar27 <mohankumar27197@gmail.com>
2017-02-11 20:58:22 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -29,8 +29,8 @@ submit.onclick=function()
 {
   var names=['name1','name2','name3','name4'];
   var list='';
-  for(var i=0;i<names.length.i++){
-      list='<li>'+names[i]+'</li>';
+  for(var i=0;i<names.length;i++){
+      list+= '<li>' + names[i] + '</li>';
   }
   var listelement=document.getElementById('name_list');
   listelement.innerHTML=list;
