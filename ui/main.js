afd6f415cd9616efe2a34a09e587e36b5b44a4a5
mohankumar27 <mohankumar27197@gmail.com>
2017-02-11 20:56:38 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -22,3 +22,16 @@ request.open('GET','http://mohankumar27.imad.hasura-app.io/counter',true);
 request.send(null);
 };
 
+var inputname=document.getElementById('namecontent');
+var name=inputname.value;
+var submit=document.getElementById('submit_btn');
+submit.onclick=function()
+{
+  var names=['name1','name2','name3','name4'];
+  var list='';
+  for(var i=0;i<names.length.i++){
+      list='<li>'+names[i]+'</li>';
+  }
+  var listelement=document.getElementById('name_list');
+  listelement.innerHTML=list;
+};
