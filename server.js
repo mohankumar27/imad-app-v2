47767c4a480367ac6ab8cf0fb69fe988261e8dfa
mohankumar27 <mohankumar27197@gmail.com>
2017-02-12 07:59:53 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -75,6 +75,14 @@ app.get('/counter',function(req,res){
     res.send(counter.toString());
 });
 
+var names=[];
+app.get('/submit?name',function(req,res)
+{
+    var name=req.query.name;
+    names.push(name);
+    req.send(JSON.stringify(names));
+});
+
 app.get('/:articleName', function (req, res) {
     var articleName=req.params.articleName;
   res.send(createTemplate(articles[articleName]));
