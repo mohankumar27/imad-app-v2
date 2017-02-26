5832730d7ccb5a426f9924ca118a35b94460830e
mohankumar27 <mohankumar27197@gmail.com>
2017-02-26 09:20:15 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -104,9 +104,21 @@ app.get('/submit-name',function(req,res)
     res.send(JSON.stringify(names));
 });
 
-app.get('/:articleName', function (req, res) {
-    var articleName=req.params.articleName;
-  res.send(createTemplate(articles[articleName]));
+app.get('/articles/:articleName', function (req, res) {
+   // var articleName=req.params.articleName;
+    pool.query("SELECT * FROM article WHERE title='"+req.params.articleName+"'", function(err,result){
+        if(err){
+            res.status(500).send(err.toString());
+        }else{
+            if(result.rows.length===0){
+                res.status(404).send("article not found");
+            }else{
+                var articleData=result.rows[0];
+                res.send(createTemplate(articles[articleName]));
+            }
+        }
+    });
+  
 });
 
 
