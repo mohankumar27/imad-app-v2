abb24374258ed421f0e1d49535d81be150cfd09b
mohankumar27 <mohankumar27197@gmail.com>
2017-03-13 10:53:06 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -81,7 +81,7 @@ return htmlTemplate;
 
 function hash(input,salt){
     var hashed=crypto.pbkdf2Sync(input, salt, 10000, 512, 'sha512');
-    return hashed.toString('hex');
+    return ["pbkdf2Sync","10000",salt,hashed.toString('hex')].join('$');
 }
 
 
