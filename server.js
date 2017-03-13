16fd89bb1ad13207a2fadf906c219bcc3b38a92d
mohankumar27 <mohankumar27197@gmail.com>
2017-03-13 11:36:35 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -92,7 +92,7 @@ app.get('/hash/:input',function(req,res){
     res.send(hashedstring);
 });
 
-app.post('create-user',function(req,res){
+app.post('/create-user',function(req,res){
     
     var username=req.body.username;
     var password=req.body.password;
